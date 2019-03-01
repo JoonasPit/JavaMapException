5207c870bf1adeabbca898f9da0e74a4414c1966
JoonasPit <43230082+JoonasPit@users.noreply.github.com>
2019-03-01 13:20:26 +0200

    Update KokonaislukuOhjelma.java

--- KokonaislukuOhjelma.java
+++ KokonaislukuOhjelma.java
@@ -1,32 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.List;
 
 public class KokonaislukuOhjelma {
+	
+	
+	public static void askNum() {
+		
+	}
 
 	public static void main(String[] args) {
 		// Reads User input 
+		List <Integer> numList = new ArrayList<Integer>();
 		Scanner input = new Scanner(System.in);
 		int toNbr;
 		String userInput;
 		boolean retry = false;
+		int i = 0;
 		
 		
-		for(int i = 1; i <= 3; i++) {
-			System.out.print("Anna " + i + ". luku: ");
-			while(!retry) {
+		do {
+			System.out.print("Anna " + (i+1) + ". luku: ");
 			try{
-				userInput = input.nextLine();
+				userInput = input.next();
 				toNbr = Integer.parseInt(userInput);
+				i++;
 				retry = true;
+				numList.add(toNbr);
 			}
 			catch(NumberFormatException e) {
 				System.out.println("Syötä kokonaisluku numeronäppäimillä!");
-				System.out.print("Anna " + i + ". luku: ");
-				i--;
 			}
-		}
+			
+		} while(retry != true || i != 10);
+		System.out.println("LUVUT:");
+		for(int b = 0; b < numList.size(); b++) {
+			int theNum = numList.get(b);
+			System.out.print(theNum + " ");
 		}
 
 	}
 }
-// not finished.
-// loop asked integer
